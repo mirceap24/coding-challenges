6ec089f5c5e4af879503f898191f1ecce9c7e2f3
mirceap24 <24mirceap@gmail.com>
2024-07-06 17:17:17 +0300

    Step 2: read from standard input

--- build-your-own-cat/ccat.c
+++ build-your-own-cat/ccat.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void cat_file(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
-        exit(1);
-    }
-
+void cat_stream(FILE *stream) {
     int ch; 
-    while ((ch = fgetc(file)) != EOF) {
+    while ((ch = fgetc(stream)) != EOF) {
         putchar(ch);
     }
+}
 
-    fclose(file);
+void cat_file(const char *filename) {
+    if (filename == NULL || strcmp(filename, "-") == 0) {
+        cat_stream(stdin);
+    } else {
+        FILE *file = fopen(filename, "r");
+        if (file == NULL) {
+            fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
+            exit(1);
+        }
+        cat_stream(file);
+        fclose(file);
+    }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
+    if (argc == 1) {
+        cat_file(NULL);
+    } else {
+        for (int i = 1; i < argc; i ++) {
+            cat_file(argv[i]);
+        }
     }
-
-    cat_file(argv[1]);
     return 0;
 }
