b6df74c2cb5bbf8fbc574e8da2e14a06be52e225
mirceap24 <24mirceap@gmail.com>
2024-07-06 17:03:31 +0300

    Step 1: open the file and write to standard output

---
+++ build-your-own-cat/ccat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cat_file(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
+        exit(1);
+    }
+
+    int ch; 
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    cat_file(argv[1]);
+    return 0;
+}
