9f524db3b77e3e882c9e4d2974dbda3cd42a3266
mirceap24 <24mirceap@gmail.com>
2024-09-03 21:56:59 +0300

    FixedWindowRateLimiter class implementation

--- build-your-own-rate-limiter/fixed_window_counter.py
+++ build-your-own-rate-limiter/fixed_window_counter.py
@@ -1,47 +1,56 @@
 # This algorithm tracks the number of requests in fixed time windows (e.g, 60-second windows)
 # and limits the number of requests that can be made within each window
 
-from math import floor
-from flask import Flask, request, jsonify, make_response
+from math import floor 
+from flask import Flask, request, jsonify 
 from time import time 
 import threading 
 
 app = Flask(__name__)
 
-# Fixed window parameters 
-WINDOW_SIZE = 60 
-REQUEST_LIMIT = 60 # 60 requests per window
-request_counters = {}
-lock = threading.Lock()
+class FixedWindowRateLimiter: 
+    def __init__(self, window_size, request_limit):
+        self.window_size = window_size 
+        self.request_limit = request_limit
+        self.request_counters = {}
+        self.lock = threading.Lock()
+    
+    def get_window_start_time(self):
+        """
+        Returns the start time of the current window.
+        """
+        current_time = time()
+        return floor(current_time / self.window_size) * self.window_size
+    
+    def allow_request(self, ip):
+        """
+        Determines if a request is allowed based on the current window and the request limit.
+        """
+        current_window = self.get_window_start_time()
 
-def get_window_start_time():
-    """
-    Returns the start time of the current window
-    """
-    current_time = time()
-    return floor(current_time / WINDOW_SIZE) * WINDOW_SIZE
+        with self.lock: 
+            if ip not in self.request_counters: 
+                self.request_counters[ip] = {"window": current_window, "count": 0}
+            
+            counter = self.request_counters[ip]
 
-def allow_request(ip):
-    current_window = get_window_start_time()
+            # if the request is in a new window, reset counter 
+            if counter["window"] != current_window: 
+                counter["window"] = current_window
+                counter["count"] = 0 
+            
+            # increment counter and check if the request is allowed 
+            if counter["count"] < self.request_limit: 
+                counter["count"] += 1 
+                return True 
+            else: 
+                return False
 
-    with lock: # thread safe access to request_counters
-        if ip not in request_counters:
-            request_counters[ip] = {"window": current_window, "count": 0}
-        
-        counter = request_counters[ip]
+# Initialize the FixedWindowRateLimiter with the desired window size and request limit
+WINDOW_SIZE = 60  # 60 seconds window
+REQUEST_LIMIT = 60  # 60 requests per window
 
-        # if the request is in a new window, reset the counter 
-        if counter["window"] != current_window: 
-            counter["window"] = current_window
-            counter["count"] = 0 
-        
-        # increment the counter and check if the request is allowed 
-        if counter["count"] < REQUEST_LIMIT: 
-            counter["count"] += 1 
-            return True 
-        else: 
-            return False 
-        
+rate_limiter = FixedWindowRateLimiter(WINDOW_SIZE, REQUEST_LIMIT)
 
 @app.route('/unlimited', methods=['GET'])
 def unlimited():
@@ -51,8 +60,8 @@ def unlimited():
 def limited():
     ip = request.remote_addr
 
-    if allow_request(ip):
-        return "Limited, don't over use me!", 200
+    if rate_limiter.allow_request(ip):
+        return "Limited, don't overuse me!", 200
     else:
         return jsonify({"error": "Too many requests"}), 429
 
