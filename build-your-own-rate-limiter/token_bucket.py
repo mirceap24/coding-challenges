bcb650ee2ac27833ca64508493bd2d3fa553424e
mirceap24 <24mirceap@gmail.com>
2024-09-03 08:35:13 +0300

    Token Bucket algorithm

---
+++ build-your-own-rate-limiter/token_bucket.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify, make_response
+from time import time 
+import threading 
+
+app = Flask(__name__)
+
+# Token bucket parameters 
+TOKENS_PER_SECOND = 1 
+BUCKET_CAPACITY = 10 
+token_buckets = {} 
+lock = threading.Lock()
+
+def get_bucket(ip):
+    current_time = time()
+
+    with lock: # thread safe access to token_buckets
+        if ip not in token_buckets: 
+            token_buckets[ip] = {"tokens": BUCKET_CAPACITY, "last_checked": current_time}
+        
+        bucket = token_buckets[ip]
+        time_passed = current_time - bucket['last_checked']
+        bucket['tokens'] = min(BUCKET_CAPACITY, bucket['tokens'] + time_passed * TOKENS_PER_SECOND)
+        bucket['last_checked'] = current_time
+    
+    return bucket 
+
+def allow_request(ip):
+    bucket = get_bucket(ip)
+
+    with lock: 
+        if bucket['tokens'] >= 1: 
+            bucket['tokens'] -= 1
+            return True 
+        else: 
+            return False 
+
+@app.route('/unlimited', methods = ['GET'])
+def unlimited():
+    return "Unlimited! Let's Go!", 200 
+
+@app.route('/limited', methods = ['GET'])
+def limited():
+    ip = request.remote_addr
+
+    if allow_request(ip):
+        return "Limited, don't over use me!", 200 
+    else: 
+        return jsonify({"error": "Too many requests"}), 429 
+
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 8080)
