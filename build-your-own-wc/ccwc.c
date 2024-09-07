ff994262b6cf40cd2b70f319224cb57766873849
mirceap24 <24mirceap@gmail.com>
2024-09-07 14:35:28 +0300

    Step 3: Output number of words in a file

--- build-your-own-wc/ccwc.c
+++ build-your-own-wc/ccwc.c
@@ -52,6 +52,40 @@ long count_lines(const char *filename) {
     return line_count;
 }
 
+long count_words(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    long word_count = 0; 
+    int in_word = 0; 
+    int ch;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (isspace(ch)) {
+            // if the current character is whitespace
+            // mark that we're no longer inside a word
+            in_word = 0;
+        } else if (in_word == 0) {
+            // if current char is not whitespace and we weren't in a word
+            // mark that we're inside a word now
+            in_word = 1; 
+            word_count++;   // increment word_count
+        }
+    }
+
+    if (ferror(file)) {
+        perror("Error reading file");
+        fclose(file);
+        return -1;
+    }
+
+    fclose(file);
+    return word_count;
+}
+
 int main(int argc, char *argv[]) {
     // Check if we have the correct number of arguments
     if (argc != 3) {
@@ -67,7 +101,10 @@ int main(int argc, char *argv[]) {
         result = count_bytes(filename);
     } else if (strcmp(option, "-l") == 0) {
         result = count_lines(filename);
-    } else {
+    } else if (strcmp(option, "-w") == 0) {
+        result = count_words(filename);
+    }
+    else {
         fprintf(stderr, "Error: Unsuported option.");
         return 1;
     }
